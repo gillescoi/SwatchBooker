74d8123e8b72472fa746e5f62e38d298c11b95a8
Gilles Coissac <info@gillescoissac.fr>
2020-09-01 11:36:02 +0200

    Convert to Python3 and refactor code.

--- src/swatchbook/websvc/__init__.py
+++ src/swatchbook/websvc/__init__.py
@@ -1,42 +1,71 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 #
 #       Copyright 2008 Olivier Berten <olivier.berten@gmail.com>
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 3 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 #
+from abc import ABC, abstractmethod
+import importlib
+from swatchbook.color import dirpath
 
-from __future__ import division
-from urllib import quote_plus,unquote_plus,urlencode,urlretrieve
-from urllib2 import urlopen,Request
-import xml.etree.cElementTree as etree
-from xml.sax.saxutils import escape as xmlescape
-from xml.sax.saxutils import unescape as xmlunescape
-from swatchbook import *
-from string import *
-from swatchbook.codecs import idfromvals
 
-class WebSvc(object):
-	about = False
+class WebSvc(ABC):
+    @property
+    @staticmethod
+    @abstractmethod
+    def about():
+        return ''
 
-for websvc in os.listdir((dirpath(__file__) or ".")):
-	if os.path.splitext(websvc)[1] == '.py' and websvc not in ('__init__.py','template.py'):
-		exec 'from '+os.path.splitext(websvc)[0]+' import *'
+    @property
+    @staticmethod
+    @abstractmethod
+    def content():
+        return []
+
+    @property
+    @staticmethod
+    @abstractmethod
+    def nbLevels():
+        return 0
+
+    @property
+    @staticmethod
+    @abstractmethod
+    def url():
+        return None
+
+    @abstractmethod
+    def level0(self):
+        pass
+
+    @abstractmethod
+    def read(self, *args):
+        pass
+
+
+for websvc in dirpath(__file__).iterdir():
+    if websvc.match('*.py') and websvc.name not in ('__init__', 'template'):
+        module = importlib.import_module(websvc.name)
+        websvc_cls = (attr for attr in dir(module) if issubclass(attr, WebSvc))
+        if websvc_cls:
+            globals()[websvc_cls.__name__] = websvc_cls
+
 
 members = {}
 
 for websvc in WebSvc.__subclasses__():
-	members[websvc.__name__] = websvc.__doc__
+    members[websvc.__name__] = websvc.__doc__
